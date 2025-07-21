d07555609fe3bcb2c0aaa994088ed4be441fb524
Jluisfeltrer <jluisfeltrer.pm@gmail.com>
2025-07-21 15:59:05 -0400

    server

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI #1 import FastAPI
+
+app = FastAPI(
+    title="AVA",  
+    description="My first agent",
+    version="1.0.0",
+    openapi_tags=[{"name": "example", "description": "An example tag"}]
+
+) # 2 instancia app
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, Jose!"}
