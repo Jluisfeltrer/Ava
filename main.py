e6f0838fa7cb479ab3ce59ab1f5c8f15aada93b7
Jluisfeltrer <jluisfeltrer.pm@gmail.com>
2025-07-23 17:04:48 -0400

    ava3

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import cohere
 from dotenv import load_dotenv
 import os
 from pydantic import BaseModel
-from langchain_cohere import ChatCohere
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+#from langchain_cohere import ChatCohere
+#from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 
 load_dotenv()
@@ -15,16 +15,17 @@ app = FastAPI(
     title="AVA",  
     description="My first agent",
     version="1.0.0",
-    openapi_tags=[{"name": "example", "description": "An example tag"}]) # 2 instancia app
+    openapi_tags=[{"name": "Agile, Versatile, Accesible", "description": "Virtual Assistant"}]) # 2 instancia app
 
 class UserMessage(BaseModel):
     message : str
 
+
 @app.post("/chat")
-async def chat(user_message : UserMessage):
-    response = co.generate(
-        model = 'command', 
-        prompt=user_message.message,
-        max_tokens=100
-    )
-    return {'response': response.generations}
+#async def chat(user_message : UserMessage):
+    #response = co.generate(
+        #model = 'command', 
+        #prompt=user_message.message,
+        #max_tokens=100
+    #)
+    #return {'response': response.generations}
